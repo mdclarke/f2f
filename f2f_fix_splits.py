084f177673bc87e445636e50736ac388bb65d96c
Maggie Clarke <mdclarke@uw.edu>
2020-06-23 11:12:33 -0700

    Update f2f_fix_splits.py

--- f2f_fix_splits.py
+++ f2f_fix_splits.py
@@ -8,17 +8,17 @@ import os.path as op
 import mne
 
 subjects = ['f2f_009_01_OTP', 'f2f_010_01_OTP', 'f2f_011_01_OTP',
-        'f2f_012_01_OTP', 'f2f_013_01_OTP', 'f2f_014_01_OTP',
-        'f2f_015_01_OTP', 'f2f_018_01_OTP', 'f2f_021_01_OTP',
-        'f2f_022_01_OTP', 'f2f_023_01_OTP', 'f2f_024_01_OTP',
-        'f2f_025_01_OTP', 'f2f_026_01_OTP', 'f2f_027_01_OTP',
-        'f2f_028_01_OTP', 'f2f_029_01_OTP', 'f2f_034_01_OTP',
-        'f2f_036_01_OTP', 'f2f_037_01_OTP', 'f2f_038_01_OTP',
-        'f2f_102_OTP', 'f2f_103_OTP', 'f2f_104_OTP',
-        'f2f_106_OTP', 'f2f_107_OTP', 'f2f_108_OTP',
-        'f2f_111_OTP', 'f2f_112_OTP', 'f2f_114_OTP',
-        'f2f_115_OTP', 'f2f_116_OTP', 'f2f_117_01_OTP',
-        'f2f_118_01_OTP', 'f2f_119_01_OTP']
+            'f2f_012_01_OTP', 'f2f_013_01_OTP', 'f2f_014_01_OTP',
+            'f2f_015_01_OTP', 'f2f_018_01_OTP', 'f2f_021_01_OTP',
+            'f2f_022_01_OTP', 'f2f_023_01_OTP', 'f2f_024_01_OTP',
+            'f2f_025_01_OTP', 'f2f_026_01_OTP', 'f2f_027_01_OTP',
+            'f2f_028_01_OTP', 'f2f_029_01_OTP', 'f2f_034_01_OTP',
+            'f2f_036_01_OTP', 'f2f_037_01_OTP', 'f2f_038_01_OTP',
+            'f2f_102_OTP', 'f2f_103_OTP', 'f2f_104_OTP',
+            'f2f_106_OTP', 'f2f_107_OTP', 'f2f_108_OTP',
+            'f2f_111_OTP', 'f2f_112_OTP', 'f2f_114_OTP',
+            'f2f_115_OTP', 'f2f_116_OTP', 'f2f_117_01_OTP',
+            'f2f_118_01_OTP', 'f2f_119_01_OTP']
 
 for subj in subjects:
     fname_2 = op.join(subj, 'raw_fif', subj + '_raw-1.fif')
